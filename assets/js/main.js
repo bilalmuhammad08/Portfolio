08e88714d518b8c52587303992eb18a1fee65332
bilalmuhammad08 <bilal-muhammad@BILAL.MUHAMMAD>
2024-08-14 11:10:57 +0530

    message button was changed

--- assets/js/main.js
+++ assets/js/main.js
@@ -253,5 +253,6 @@
 
 let sendMessage = document.getElementById('send_message')
 sendMessage.addEventListener("click", function(){
-  windows.location.href = "https://wa.me/8921876193"
+  window.location.href = "https://wa.me/8921876193"
 })
+
